ff6f1de04fb70714dc094c7dcadc2b93b3481f96
Jun He <jhe@cs.wisc.edu>
2014-10-27 00:00:11 -0500

    able to print ROC coords

--- hw2-ann/main.py
+++ hw2-ann/main.py
@@ -9,6 +9,9 @@ import random
 classdic = {'Rock':0,
             'Mine':1}
 
+classdic2 = {'Rock':'neg',
+            'Mine':'pos'}
+
 def pretty_print(example_table):
     width = 15
     header = [x.ljust(width) for x in get_fieldnames(example_table)]
@@ -106,14 +109,57 @@ def ann_test(datatable, wlist):
         outvec.append( get_output(row, wlist) )
 
     yvec = get_column(datatable, 'Class')
-    outvec = [num2class(y) for y in outvec]
-    #print yvec
-    #print outvec
-    comp = [x1==x2 for x1,x2 in zip(yvec, outvec)]
+    outvecclass = [num2class(y) for y in outvec]
+    comp = [x1==x2 for x1,x2 in zip(yvec, outvecclass)]
     
     accuracy = float(sum(comp))/len(comp)
     #print accuracy
-    return accuracy
+    d = {'accuracy':accuracy,
+         'numout'  :outvec,
+         'classout':outvecclass,
+         'correctclass':yvec}
+    return d
+
+def roc_points(instances):
+    "instances are in the form of [(c,y),...]"
+    # sort
+    instances = sorted(instances, key=lambda x: x[0]) # sort by c 
+
+    TP = 0
+    FP = 0
+    last_TP = 0
+
+    c = [x[0] for x in instances]
+    y = [x[1] for x in instances]
+
+    num_neg = len( [x for x in y if x=='neg'] )
+    num_pos = len( [x for x in y if x=='pos'] )
+    
+    m = len(instances)
+
+    coords_list = [(0,0)]
+    for i in range(1, m):
+        if c[i] != c[i-1] and y[i] == 'neg' and TP > last_TP:
+            FPR = float(FP)/num_neg
+            TPR = float(TP)/num_pos
+            coords_list.append((FPR, TPR))
+            last_TP = TP
+
+        if y[i] == 'pos':
+            TP += 1
+        else:
+            FP += 1
+
+    FPR = float(FP)/num_neg
+    TPR = float(TP)/num_pos
+    coords_list.append((FPR, TPR))
+
+    print 'x','y'
+    for x,y in coords_list:
+        print x,y
+
+
+       
 
 if __name__ == '__main__':
     argv = sys.argv
@@ -158,8 +204,12 @@ if __name__ == '__main__':
         wlist = ann_train(traintable, lrate, epochs)
         
         # test
-        accuracy = ann_test(testtable, wlist)
-        accuracylist.append(accuracy)
+        testresult = ann_test(testtable, wlist)
+        accuracylist.append(testresult['accuracy'])
+        
+        c = testresult['numout']
+        y = [classdic2[x] for x in testresult['correctclass']]
+        roc_points(zip(c,y))
 
     ave_accuracy = float(sum(accuracylist))/len(accuracylist)
     print ave_accuracy
